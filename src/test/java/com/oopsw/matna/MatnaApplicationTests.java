25380477f9b5ac3c3e0ef699cbe16cb65a6a35b1
reeuse <wodyd0810@naver.com>
2025-11-20 11:51:40 +0900

    initial setting

---
+++ src/test/java/com/oopsw/matna/MatnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.matna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
