48ca2a0743dc13b907af1b5e6d8eb8ded528031c
wldusdn <wldusdngg@naver.com>
2025-11-22 21:36:33 +0900

    feat: Added editMyProfileTest

---
+++ src/main/java/com/oopsw/matna/vo/MemberVO.java
@@ -0,0 +1,24 @@
+package com.oopsw.matna.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Builder
+@Data
+public class MemberVO {
+    private Integer memberNo;
+    private String memberId;
+    private String password;
+    private String accountName;
+    private String nickname;
+    private String bank;
+    private String accountNumber;
+    private LocalDateTime inDate;
+    private LocalDateTime delDate;
+    private String roll;
+    private LocalDateTime banDate;
+    private Integer point;
+    private String imageUrl;
+    private String address;
+}
--- src/test/java/com/oopsw/matna/repository/MemberRepositoryTests.java
+++ src/test/java/com/oopsw/matna/repository/MemberRepositoryTests.java
@@ -2,6 +2,7 @@ package com.oopsw.matna.repository;
 
 import com.oopsw.matna.repository.entity.Member;
 import com.oopsw.matna.vo.MemberProfileVO;
+import com.oopsw.matna.vo.MemberVO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,6 +30,7 @@ public class MemberRepositoryTests {
         System.out.println(MemberProfileVO.builder().nickname(m.getNickname()).imageUrl(m.getImageUrl()).point(m.getPoint()).build());
     }
 
+
 //    @Test
 //    @Transactional
 //    @Commit
@@ -51,6 +53,55 @@ public class MemberRepositoryTests {
                 .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
         boolean isMatched = bCryptPasswordEncoder.matches(password, m.getPassword());
         System.out.println(isMatched);
+    }
+
+    @Test
+    public void getMemberTest() {
+        Integer memberNo = 5;
+        Member m = memberRepository.findById(memberNo)
+                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
+        MemberVO member = MemberVO.builder()
+                .memberNo(m.getMemberNo())
+                .memberId(m.getMemberId())
+                .accountName(m.getAccountName())
+                .nickname(m.getNickname())
+                .bank(m.getBank())
+                .accountNumber(m.getAccountNumber())
+                .inDate(m.getInDate())
+                .delDate(m.getDelDate())
+                .roll(m.getRoll())
+                .banDate(m.getBanDate())
+                .point(m.getPoint())
+                .imageUrl(m.getImageUrl())
+                .address(m.getAddress())
+                .build();
+        System.out.println(member);
+    }
+
+    @Test
+    @Transactional
+    @Commit
+    public void editMemberProfileTest() {
+        MemberVO editMember = MemberVO.builder()
+                .memberNo(5)
+                .nickname("마리오")
+                .password("member_1")
+                .imageUrl("mario.jpg")
+                .bank("농협은행")
+                .accountNumber("302-1234-5678-01")
+                .accountName("김찰스")
+                .address("서울시 금천구 독산동 ")
+                .build();
+
+        Member m = memberRepository.findById(editMember.getMemberNo())
+                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
+        m.setNickname(editMember.getNickname());
+        m.setPassword(bCryptPasswordEncoder.encode(editMember.getPassword()));
+        m.setImageUrl(editMember.getImageUrl());
+        m.setBank(editMember.getBank());
+        m.setAccountNumber(editMember.getAccountNumber());
+        m.setAccountName(editMember.getAccountName());
+        m.setAddress(editMember.getAddress());
     }
 
 }
