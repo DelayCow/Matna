b3a455d5ccaa4459e26de5d55eb55006f0d2fa4e
shpark091 <shpark091@gmail.com>
2025-11-22 12:57:58 +0900

    feat : Added addIngredient in GroupBuy

---
+++ src/main/java/com/oopsw/matna/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.oopsw.matna.repository;
+
+import com.oopsw.matna.repository.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Member findByMemberNo(int memberNo);
+}
--- src/test/java/com/oopsw/matna/repository/GroupBuyRepositoryTests.java
+++ src/test/java/com/oopsw/matna/repository/GroupBuyRepositoryTests.java
@@ -1,16 +1,23 @@
 package com.oopsw.matna.repository;
 
+import com.oopsw.matna.repository.entity.GroupBuy;
 import com.oopsw.matna.repository.entity.Ingredient;
+import com.oopsw.matna.repository.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
 public class GroupBuyRepositoryTests {
     @Autowired
     IngredientRepository ingredientRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    GroupBuyRepository groupBuyRepository;
 
     @Test
     void searchIngredientKeyword(){
@@ -19,5 +26,22 @@ public class GroupBuyRepositoryTests {
         for (Ingredient ingredient : results) {
             System.out.println(ingredient.getIngredientName());
         }
+    }
+    @Test
+    void addIngredient(){
+        Member creatorMember = memberRepository.findById(5).get();
+        Ingredient newIngredient = ingredientRepository.save(
+                Ingredient.builder()
+                .ingredientName("모닝빵")
+                .creator(creatorMember)
+                .inDate(LocalDateTime.now())
+                .build());
+        System.out.println(newIngredient.getIngredientName());
+    }
+    @Test
+    void addGroupBuyPeriod(){
+        GroupBuy newGroupBuy = groupBuyRepository.save(
+                GroupBuy.builder().in.build()
+        )
     }
 }
