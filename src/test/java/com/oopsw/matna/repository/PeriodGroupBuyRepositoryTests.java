5769a2fd6c0bbcd752a946039a2d2bdff074f2b6
shpark091 <shpark091@gmail.com>
2025-11-23 22:53:02 +0900

    wip : fix Tests name

--- src/test/java/com/oopsw/matna/repository/PeriodGroupBuyRepositoryTests.java
+++ src/test/java/com/oopsw/matna/repository/PeriodGroupBuyRepositoryTests.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @SpringBootTest
 @Transactional
-public class GroupBuyRepositoryTests {
+public class PeriodGroupBuyRepositoryTests {
     @Autowired
     IngredientRepository ingredientRepository;
     @Autowired
