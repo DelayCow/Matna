216b2d02dce0942a6b88647d7953e6e584d59433
reeuse <wodyd0810@naver.com>
2025-11-24 04:35:14 +0900

    feat: added userManagement

---
+++ src/main/java/com/oopsw/matna/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.oopsw.matna.repository;
+
+import com.oopsw.matna.repository.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByMemberId(String memberId);
+    //service에서 passwordEncoder.matches(rawPassword, encodedPassword) 하기
+    boolean existsByNickname(String nickname);
+
+    List<Member> findAllByRollOrderByMemberNoDesc(String roll);
+    List<Member> findByNicknameContaining(String nickname);
+    List<Member> findByMemberIdContaining(String memberId);
+    List<Member> findAllByRollAndInDateBetween(String role, LocalDateTime start, LocalDateTime end);
+}
---
+++ src/test/java/com/oopsw/matna/MemberRepositoryTests.java
@@ -0,0 +1,52 @@
+package com.oopsw.matna;
+
+
+import com.oopsw.matna.repository.MemberRepository;
+import com.oopsw.matna.repository.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void findAllByRoll() {
+        System.out.println(memberRepository.findAllByRollOrderByMemberNoDesc("USER"));
+    }
+
+    @Test
+    public void findByNickname() {
+        System.out.println(memberRepository.findByNicknameContaining("감자"));
+    }
+
+    @Test
+    public void findByMemberId() {
+        System.out.println(memberRepository.findByMemberIdContaining("member_1"));
+    }
+
+    @Test
+    public void findAllByRollAndInDateBetween(){
+        LocalDateTime start = LocalDateTime.of(2025, 11, 20, 0, 0);
+        LocalDateTime end   = LocalDateTime.of(2025, 11, 24, 23, 59);
+        System.out.println(memberRepository.findAllByRollAndInDateBetween("USER", start, end));
+    }
+
+    @Test
+    @Transactional
+    public void updateBanDate() {
+        Member member = memberRepository.findByMemberId("member_18")
+                .orElseThrow(() -> new RuntimeException("회원이 존재하지 않습니다."));
+        LocalDateTime banDate = LocalDateTime.of(2025, 11, 25, 0, 0);
+        member.setBanDate(banDate);
+
+        //@Transactional 안에서 수정했으므로 save() 없이도 DB 반영됨
+    }
+}
