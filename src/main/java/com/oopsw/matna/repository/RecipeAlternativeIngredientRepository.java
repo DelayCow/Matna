1b94f5095fddcad88ce59f7893db05ee09ca001f
June <bhan40936@gmail.com>
2025-11-24 16:17:05 +0900

    fix : exterminate all @Repository

--- src/main/java/com/oopsw/matna/repository/RecipeAlternativeIngredientRepository.java
+++ src/main/java/com/oopsw/matna/repository/RecipeAlternativeIngredientRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface RecipeAlternativeIngredientRepository extends CrudRepository<RecipeAlternativeIngredient, Integer> {
 
     List<RecipeAlternativeIngredient> findByReview_Recipe_RecipeNo(Integer recipeNo);
