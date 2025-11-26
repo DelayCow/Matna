62929b050ebcb25c0dff520f794dbc98e9b48612
shpark091 <shpark091@gmail.com>
2025-11-26 17:34:22 +0900

    fix : Fixed GroupBuys DAOTests

--- src/main/java/com/oopsw/matna/repository/MemberRepository.java
+++ src/main/java/com/oopsw/matna/repository/MemberRepository.java
@@ -3,7 +3,6 @@ package com.oopsw.matna.repository;
 import com.oopsw.matna.repository.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +10,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     boolean existsByNickname(String nickname);
-    List<Member> memberNo(Integer memberNo);
+    Member findByMemberNo(Integer memberNo);
     Optional<Member> findByMemberId(String memberId);
 }
