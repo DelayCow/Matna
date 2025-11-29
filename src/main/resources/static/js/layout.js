62cb0b030ed82c54c0e1de9bc153dbc1a7357ce1
wldusdn <wldusdngg@naver.com>
2025-11-29 21:30:39 +0900

    style: Active menubar depending on the pagename

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -1,1 +1,13 @@
-document.querySelector()
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+    const links = document.querySelectorAll(".nav-link");
+
+    links.forEach(link => {
+        const linkPage = link.getAttribute("href").split("/").pop();
+        console.log(currentPage)
+        console.log(linkPage)
+        if (linkPage === currentPage) {
+            link.classList.add("active");
+        }
+    });
+});
