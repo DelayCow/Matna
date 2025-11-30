74a584e448f4297807aecda27e3f9499f89024a2
wldusdn <wldusdngg@naver.com>
2025-11-30 16:13:25 +0900

    feat: Added ImageStorageService

---
+++ src/main/java/com/oopsw/matna/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.oopsw.matna.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${file.upload.path:uploads}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:"+uploadPath+"/");
+    }
+}
---
+++ src/main/java/com/oopsw/matna/service/LocalImageStorageService.java
@@ -0,0 +1,42 @@
+package com.oopsw.matna.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Service
+public class LocalImageStorageService implements ImageStorageService{
+
+    @Value("${file.upload.path:uploads}")
+    private String uploadPath;
+
+    @Override
+    public String save(MultipartFile file, String directory) throws IOException {
+        Path directoryPath = Paths.get(uploadPath + directory);
+        if(!Files.exists(directoryPath)){
+            Files.createDirectories(directoryPath);
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uniqueFilename = UUID.randomUUID().toString() + extension;
+
+        Path filePath = directoryPath.resolve(uniqueFilename);
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+        return "/" + uploadPath + "/" + directory + "/" + uniqueFilename;
+    }
+
+    @Override
+    public void delete(String imageUrl) throws IOException {
+        Path filePath = Paths.get(imageUrl.substring(1));
+        Files.deleteIfExists(filePath);
+    }
+}
