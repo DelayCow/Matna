d379f45c00582c5c4ade98401386df646ac1b4db
wldusdn <wldusdngg@naver.com>
2025-12-04 14:45:31 +0900

    style: change header and menubar related groupBuy

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     links.forEach(link => {
         const linkPage = link.getAttribute("href").split("/").pop();
+        console.log(linkPage)
+        console.log(currentPage)
         if (linkPage === currentPage) {
             link.classList.add("active");
         }
