ab8edb8d7b477b3a14e175cb27cb8f00107ef54b
wldusdn <wldusdngg@naver.com>
2025-12-16 18:48:57 +0900

    refactor: Change authentication from session method to jwt method

--- src/main/java/com/oopsw/matna/vo/MemberVO.java
+++ src/main/java/com/oopsw/matna/vo/MemberVO.java
@@ -1,11 +1,15 @@
 package com.oopsw.matna.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberVO {
     private Integer memberNo;
     private String memberId;
